3e504cf6289569e941b0b602fbb7f9c4e18f8ae0
mcfstr <matt.foster.c@gmail.com>
2014-04-09 09:54:32 -0700

    cleanup url cruft

--- pdxpython/urls.py
+++ pdxpython/urls.py
@@ -4,9 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pdxpython.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
     # index page
     url(r'^$', 'apps.home.views.index', name='index'),
